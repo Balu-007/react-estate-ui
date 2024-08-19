c94a5278ca9a43b29606321c9f12a6b98d08aa8b
Balu <balubittu007@gmail.com>
2024-08-19 19:59:28 +0530

    react-router, Outlet

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,36 @@
-import Navbar from "./components/navbar/Navbar";
-import "./layout.scss";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./routes/homepage/HomePage";
+import ListPage from "./routes/listPage/ListPage";
+import Layout from "./routes/layout/Layout";
+import Login from "./routes/login/Login";
+import SinglePage from "./routes/singlePage/SinglePage";
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <HomePage />
+        },
+        {
+          path: "/list",
+          element: <ListPage />
+        },
+        {
+          path: "/login",
+          element: <Login />
+        },
+        {
+          path: "/:id",
+          element: <SinglePage />
+        }
+      ]
+    }
+  ]);
   return (
-    <div className="layout">
-      <div className="navbar">
-        <Navbar />
-      </div>
-
-      <div className="content">
-        <HomePage />
-      </div>
-      
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
